93565b4d1c50d4e9e46b4ed2c6e681a63cf76e6c
Ben Rowe <brow495@gmail.com>
2020-10-08 15:54:46 -0600

    Create mirror function no mutation. 10820

--- js/10820.js
+++ js/10820.js
@@ -1,5 +1,6 @@
 function mirror(data) {
-    let answer = data
+    let answer = []
+    data.forEach(number => answer = [...answer, number])
     answer.sort((a,b) => a - b)
     let flip = answer.slice(0, answer.length - 1)
     return answer.concat(flip.reverse())
