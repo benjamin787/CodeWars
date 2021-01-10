4f0bd84e8dc69cfd21aac2fa76c4d1e3691015e4
Ben Rowe <brow495@gmail.com>
2021-01-09 23:24:07 -0700

    Improve regexp syntax.

--- js/010921.js
+++ js/010921.js
@@ -2,3 +2,6 @@ function alphanumeric(text) {
     let regex = /([^a-zA-Z0-9]|\s)/
     return !regex.test(text) && text.length > 0
 }
+
+//ooOOOooOoOOo so much prettier
+const alphanumeric = text => /^[a-z0-9]+$/i.test(text)
